86e6dc242f915cd5cf8405bd663306a826ce8598
Sala K404 <salak404@itmsalas.edu.co>
2022-04-01 17:12:45 -0500

    Mi primer commit

---
+++ Suma.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+using namespace std;
+
+int a,b;
+int suma(int a, int b);
+
+		int c=a+b;
+		
+		
+int main(){
+	
+	cout<<"Ingrese el primer dìgito de la operaciòn: "<<endl;cin>>a;
+	cout<<"Ingrese el segundo dìgito: ";cin>>b;
+	
+	int suma(int a, int b);
+	int c=a+b;
+	
+	return c;
+	
+	
+}
+
